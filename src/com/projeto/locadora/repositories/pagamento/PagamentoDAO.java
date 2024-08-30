d1c163dbadfd009bf3ad8f9ad73a58dcf923f3b1
Gabriel de Pauli Santos - AQ302282X <gabriel.pauli@aluno.ifsp.edu.br>
2024-08-30 15:25:50 -0300

    Alterando repositorios de Pagamento

--- src/com/projeto/locadora/repositories/pagamento/PagamentoDAO.java
+++ src/com/projeto/locadora/repositories/pagamento/PagamentoDAO.java
@@ -1,5 +1,11 @@
 package com.projeto.locadora.repositories.pagamento;
 
-public interface PagamentoDAO {
+import com.projeto.locadora.entities.pagamento.Pagamento;
+import java.util.List;
+import java.util.Optional;
 
+public interface PagamentoDAO {
+    Optional<Pagamento> encontrarPagamentoPorId(int id); 
+    void cadastrarPagamento(Pagamento pagamento);
+    List<Pagamento> getAllPagamentos();
 }
--- src/com/projeto/locadora/repositories/pagamento/PagamentoDAOImp.java
+++ src/com/projeto/locadora/repositories/pagamento/PagamentoDAOImp.java
@@ -1,5 +1,35 @@
 package com.projeto.locadora.repositories.pagamento;
 
+import com.projeto.locadora.entities.pagamento.Pagamento;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 public class PagamentoDAOImp implements PagamentoDAO {
+    private static final List<Pagamento> pagamentos = new ArrayList<>();
+    private static final PagamentoDAOImp repositorio = new PagamentoDAOImp();
+
+    @Override
+    public Optional<Pagamento> encontrarPagamentoPorId(int id) {
+        Optional<Pagamento> pagamento = pagamentos.stream()
+                .filter(pag -> pag.getCodigo() == id)
+                .findFirst();
+        
+        return pagamento;
+    }
+
+    @Override
+    public void cadastrarPagamento(Pagamento pagamento) {
+        pagamentos.add(pagamento);
+    }
+
+    @Override
+    public List<Pagamento> getAllPagamentos() {
+        return Collections.unmodifiableList(pagamentos);
+    }
     
+    public static PagamentoDAOImp getInstance() {
+        return repositorio;
+    }
 }
